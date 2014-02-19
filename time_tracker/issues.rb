be602f4a084bdd46a0c5a1354ecc69d37a72349f
Stephen Russett <stephenrussett@gmail.com>
2014-02-19 17:28:45 -0500

    Removed accepted time comment emoji as it was moved into another file

--- time_tracker/issues.rb
+++ time_tracker/issues.rb
@@ -134,18 +134,6 @@ module Gh_Issue
 		return acceptedNonBilliableEmoji.any? { |b| commentBody =~ /#{b}/ }
 	end
 
-	# def self.accepted_time_comment_emoji(*acceptedTimeCommentEmoji)
-	# 	acceptedTimeCommentEmoji = [":clock130:", ":clock11:", ":clock1230:", ":clock3:", ":clock430:", 
-	# 							":clock6:", ":clock730:", ":clock9:", ":clock10:", ":clock1130:", 
-	# 							":clock2:", ":clock330:", ":clock5:", ":clock630:", ":clock8:", 
-	# 							":clock930:", ":clock1:", ":clock1030:", ":clock12:", ":clock230:", 
-	# 							":clock4:", ":clock530:", ":clock7:", ":clock830:"]
-	# end
-
-	# def self.accepted_nonBillable_emoji(*acceptedNonBilliableEmoji)
-	# 	acceptedNonBilliableEmoji = [":free:"]
-	# end
-
 		# processes a comment for time comment information
 	def self.process_issue_comment_for_time(issueComment)
 
