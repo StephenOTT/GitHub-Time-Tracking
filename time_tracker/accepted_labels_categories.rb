966c39988c9441f6419c587aae028982da2f5e6c
Stephen Russett <stephenrussett@gmail.com>
2014-02-19 17:30:31 -0500

    Moved the Accepted Labels categories into its own file for easier management

---
+++ time_tracker/accepted_labels_categories.rb
@@ -0,0 +1,19 @@
+
+module Accepted_Labels_Categories
+
+	def self.accepted_labels_categories
+			acceptedLabels = [
+								{:category => "Priority:", :label => "Low"},
+								{:category => "Priority:", :label => "Medium"},
+								{:category => "Priority:", :label => "High"},
+								{:category => "Size:", :label => "Small"},
+								{:category => "Size:", :label => "Medium"},
+								{:category => "Size:", :label => "Large"},
+								{:category => "Version:", :label => "1.0"},
+								{:category => "Version:", :label => "1.5"},
+								{:category => "Version:", :label => "2.0"},
+								{:category => "Task:", :label => "Medium"},
+								{:category => "Size:", :label => "Medium"},
+							]
+	end
+end
