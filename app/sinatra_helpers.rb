860f333d9e251e101e2624880e7b185aacea212e
Stephen Russett <stephenrussett@gmail.com>
2014-02-27 02:10:08 -0500

    Added helpers rb file for faster maint

---
+++ app/sinatra_helpers.rb
@@ -0,0 +1,22 @@
+require_relative '../controller'
+require_relative '../time_tracker/time_analyzer'
+
+module Sinatra_Helpers
+
+    def self.download_time_tracking_data(user,repo, githubObject)
+      userRepo = "#{user}/#{repo}" 
+      Time_Tracking_Controller.controller(userRepo, githubObject, true)
+    end
+
+
+    def self.analyze(user,repo)
+      userRepo = "#{user}/#{repo}"
+      Time_Analyzer.controller
+      spentHours = Time_Analyzer.analyze_issue_spent_hours
+      budgetHours = Time_Analyzer.analyze_issue_budget_hours
+      Time_Analyzer.merge_issue_time_and_budget(spentHours, budgetHours)
+    end
+
+
+
+end
