a2b3163d65d9bf0ab715d657194053c74c86c885
Stephen Russett <stephenrussett@gmail.com>
2014-02-28 17:27:05 -0500

    Cleaned up whitespace

--- app/sinatra_helpers.rb
+++ app/sinatra_helpers.rb
@@ -3,13 +3,13 @@ require_relative '../time_tracker/time_analyzer'
 
 module Sinatra_Helpers
 
-    def self.download_time_tracking_data(user,repo, githubObject)
+    def self.download_time_tracking_data(user, repo, githubObject)
       userRepo = "#{user}/#{repo}" 
       Time_Tracking_Controller.controller(userRepo, githubObject, true)
     end
 
 
-    def self.analyze_issues(user,repo)
+    def self.analyze_issues(user, repo)
       userRepo = "#{user}/#{repo}"
       Time_Analyzer.controller
       spentHours = Time_Analyzer.analyze_issue_spent_hours
@@ -17,7 +17,7 @@ module Sinatra_Helpers
       Time_Analyzer.merge_issue_time_and_budget(spentHours, budgetHours)
     end
 
-    def self.analyze_milestones(user,repo)
+    def self.analyze_milestones(user, repo)
       userRepo = "#{user}/#{repo}"
       Time_Analyzer.controller
       Time_Analyzer.analyze_milestones
