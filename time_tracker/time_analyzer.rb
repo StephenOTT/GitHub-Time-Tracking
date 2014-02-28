bcae2b986a0fd0129cef6efd36fb486642dfbe05
Stephen Russett <stephenrussett@gmail.com>
2014-02-27 22:19:49 -0500

    New mongo aggregation query to handle analysis of milestones

--- time_tracker/time_analyzer.rb
+++ time_tracker/time_analyzer.rb
@@ -102,6 +102,43 @@ module Time_Analyzer
 
 		return issuesTime
 	end
+
+	def self.analyze_milestones
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							milestone_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_state: 1, 
+							milestone_title: 1, 
+							milestone_open_issue_count: 1,
+							milestone_closed_issue_count: 1,
+							budget_tracking_commits:{ duration: 1, 
+													type: 1}}},			
+			{ "$match" => { type: "Milestone" }},
+			{ "$unwind" => "$budget_tracking_commits" },
+			{ "$match" => { "budget_tracking_commits.type" => { "$in" => ["Milestone Budget"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							milestone_state: "$milestone_state",
+							milestone_title: "$milestone_title",
+							milestone_open_issue_count: "$milestone_open_issue_count",
+							milestone_closed_issue_count: "$milestone_closed_issue_count",},
+							milestone_duration_sum: { "$sum" => "$budget_tracking_commits.duration" }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["milestone_duration_sum"] = x["milestone_duration_sum"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+
+
 end
 
 # start = Time_Analyzer.new
