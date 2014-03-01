c467183fccb03f27603cdf6598ecdf59a45f6152
Stephen Russett <stephenrussett@gmail.com>
2014-03-01 01:38:00 -0500

    Added mondo aggregation query to get issues and the durations spent on those issues for a specific milestone

--- time_tracker/time_analyzer.rb
+++ time_tracker/time_analyzer.rb
@@ -170,6 +170,45 @@ module Time_Analyzer
 
 
 
+	# gets the issues and the spent hours for a specific milestone
+	# argument input is a array of integers: 1 or more, representing milestone numbers
+	def self.analyze_issue_spent_hours_per_milestone(milestoneNumber)
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							issue_state: 1, 
+							issue_title: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},			
+			{ "$match" => { type: "Issue", milestone_number: milestoneNumber }},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Time"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_title: "$issue_title",
+							issue_state: "$issue_state", },
+							time_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							time_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+
+
 
 end
 
