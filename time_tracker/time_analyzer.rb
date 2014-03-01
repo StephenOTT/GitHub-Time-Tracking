5066ffea33b3a6ee89f1e24d504b9c092caa5131
Stephen Russett <stephenrussett@gmail.com>
2014-03-01 17:59:02 -0500

    New mongo aggregation query to get labels and issue time within those labels

--- time_tracker/time_analyzer.rb
+++ time_tracker/time_analyzer.rb
@@ -208,6 +208,55 @@ module Time_Analyzer
 
 
 
+	# TODO: Review code for better structure.
+	# TODO: Review code to work out problematic selections.  
+	# Not 100% sure this query wont cause bad results in certain situations.
+	def self.analyze_issue_spent_hours_per_label(category, label)
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							issue_state: 1, 
+							issue_title: 1, 
+							time_tracking_commits: { duration: 1, 
+													type: 1, 
+													comment_id: 1 },
+							labels: { category: 1,
+									  label: 1 },
+													}},			
+			{ "$match" => { type: "Issue" }},
+			{ "$unwind" => "$labels" },
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Time"] }}},
+			{ "$match" => { "labels.category" => { "$in" => category }}},
+			{ "$match" => { "labels.label" => { "$in" => label }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							category: "$labels.category",
+							label: "$labels.label",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_title: "$issue_title",
+							issue_state: "$issue_state", },
+							time_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							time_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+
+
+
 
 
 end
