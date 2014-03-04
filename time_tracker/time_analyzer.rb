479a8b4cd046a2ff5b6ef41369acffcc86ab8835
Stephen Russett <stephenrussett@gmail.com>
2014-03-04 11:49:27 -0500

    New mongo query to get the total hours spent for issues attached to a specific milestone

--- time_tracker/time_analyzer.rb
+++ time_tracker/time_analyzer.rb
@@ -133,6 +133,33 @@ module Time_Analyzer
 	end
 
 
+	def self.analyze_issue_spent_hours_for_milestone(milestoneNumber)
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},			
+			{ "$match" => { type: "Issue" }},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Time"] }}},
+			{ "$match" => { "milestone_number" => { "$in" => milestoneNumber }}},			
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number"},
+							time_duration_sum: { "$sum" => "$time_tracking_commits.duration" }}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			# x["_id"]["time_comment_count"] = x["time_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
 	# gets time per user for each issue
 	def self.analyze_issue_spent_hours_per_user(repo, issueNumber)
 
