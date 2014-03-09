30a20cbf5bce69f3c62b577e8c97d48e637a439a
Stephen Russett <stephenrussett@gmail.com>
2014-03-09 13:31:07 -0400

    Added new files for issue processing and aggregation - more modular code setup

---
+++ time_analyzer/issues_aggregation.rb
@@ -0,0 +1,121 @@
+require_relative './mongo'
+
+
+module Issues_Aggregation
+
+	def self.controller
+
+		Mongo_Connection.mongo_Connect("localhost", 27017, "GitHub-TimeTracking", "TimeTrackingCommits")
+
+	end
+
+
+	def self.analyze_issue_spent_hours
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							issue_state: 1, 
+							issue_title: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},			
+			{ "$match" => { type: "Issue" }},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Time"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_title: "$issue_title",
+							issue_state: "$issue_state", },
+							time_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							time_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+	def self.analyze_issue_spent_hours_per_user(repo, issueNumber)
+
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1,
+							issue_title: 1, 
+							milestone_number: 1,
+							_id: 1, 
+							repo: 1,
+							time_tracking_commits:{ duration: 1, 
+													type: 1,
+													work_logged_by: 1}}},			
+			{ "$match" => { type: "Issue", repo: repo, issue_number: issueNumber }},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Time"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_title: "$issue_title",
+							work_logged_by: "$time_tracking_commits.work_logged_by"},
+							time_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							time_comment_count: { "$sum" => 1 }
+							}},
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+
+
+		def self.analyze_issue_budget_hours
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							issue_state: 1, 
+							issue_title: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},			
+			{ "$match" => { type: "Issue" }},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Budget"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_state: "$issue_state",
+							issue_title: "$issue_title",},
+							budget_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							budget_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["budget_duration_sum"] = x["budget_duration_sum"]
+			x["_id"]["budget_comment_count"] = x["budget_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+	
+end
---
+++ time_analyzer/issues_processor.rb
@@ -0,0 +1,44 @@
+require_relative 'issues_aggregation'
+require_relative 'helpers'
+
+
+module Issues_Processor
+
+    def self.analyze_issues(user, repo)
+      userRepo = "#{user}/#{repo}"
+      Issues_Aggregation.controller
+      spentHours = Issues_Aggregation.analyze_issue_spent_hours
+      budgetHours = Issues_Aggregation.analyze_issue_budget_hours
+      issues = Helpers.merge_issue_time_and_budget(spentHours, budgetHours)
+      issues.each do |x|
+        if x["time_duration_sum"] != nil
+          x["time_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["time_duration_sum"], "long")
+        end
+        if x["budget_duration_sum"] != nil
+          x["budget_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["budget_duration_sum"], "long")
+        end
+      end
+
+      issues = self.process_issues_for_budget_left(issues)
+
+      return issues
+
+    end
+
+
+
+    def self.process_issues_for_budget_left(issues)
+      issues.each do |i|
+        if i["budget_duration_sum"] != nil
+          # TODO Cleanup code for Budget left.
+          budgetLeftRaw = Helpers.budget_left?(i["budget_duration_sum"], i["time_duration_sum"])
+          budgetLeftHuman = Helpers.convertSecondsToDurationFormat(budgetLeftRaw, "long")
+          i["budget_left_raw"] = budgetLeftRaw
+          i["budget_left_human"] = budgetLeftHuman
+        end
+      end
+      return issues
+    end
+
+
+end
