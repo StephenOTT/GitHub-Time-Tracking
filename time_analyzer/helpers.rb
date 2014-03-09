5ff792fa537a54104397ada8d55720c0e2afa2e2
Stephen Russett <stephenrussett@gmail.com>
2014-03-09 13:33:59 -0400

    added the merge issue time and budget method into the helpers for time_analyzer

--- time_analyzer/helpers.rb
+++ time_analyzer/helpers.rb
@@ -10,4 +10,31 @@ module Helpers
 		outputFormat = outputFormat.to_sym
 		return ChronicDuration.output(timeInSeconds, :format => outputFormat, :keep_zero => true)
 	end
+
+
+	def self.merge_issue_time_and_budget(issuesTime, issuesBudget)
+
+		issuesTime.each do |t|
+
+			issuesBudget.each do |b|
+
+				if b["issue_number"] == t["issue_number"]
+					t["budget_duration_sum"] = b["budget_duration_sum"]
+					t["budget_comment_count"] = b["budget_comment_count"]
+					break
+				end					
+			end
+			if t.has_key?("budget_duration_sum") == false and t.has_key?("budget_comment_count") == false
+				t["budget_duration_sum"] = nil
+				t["budget_comment_count"] = nil
+			end
+		end
+
+		return issuesTime
+	end
+
+
+
+
+	
 end
