38f4dbe3d0f565bcc3c2b322892d5834c4e5f861
Stephen Russett <stephenrussett@gmail.com>
2014-03-14 03:08:49 -0400

    cleaned up method fods

--- time_analyzer/issues_aggregation.rb
+++ time_analyzer/issues_aggregation.rb
@@ -94,6 +94,72 @@ module Issues_Aggregation
 							milestone_number: 1, 
 							issue_state: 1, 
 							issue_title: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},
+			{ "$match" => { repo: repo }},
+			{ "$match" => { type: "Issue" }},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Budget"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_state: "$issue_state",
+							issue_title: "$issue_title",},
+							budget_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							budget_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["budget_duration_sum"] = x["budget_duration_sum"]
+			x["_id"]["budget_comment_count"] = x["budget_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+	def self.get_issue_budget(repo, issueNumber)
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							issue_state: 1, 
+							issue_title: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},			
+			{ "$match" => { repo: repo }},			
+			{ "$match" => { type: "Issue" }},
+			{ "$match" => {issue_number: issueNumber}},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Budget"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_state: "$issue_state",
+							issue_title: "$issue_title",},
+							budget_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							budget_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["budget_duration_sum"] = x["budget_duration_sum"]
+			x["_id"]["budget_comment_count"] = x["budget_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
+
+
 							_id: 1, 
 							repo: 1,
 							time_tracking_commits:{ duration: 1, 
