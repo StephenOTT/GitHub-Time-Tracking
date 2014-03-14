58f4c1f5ebf54446795ff1b983906695cd9f8595
Stephen Russett <stephenrussett@gmail.com>
2014-03-14 03:08:04 -0400

    new method for dealing with issue time

--- time_analyzer/issues_aggregation.rb
+++ time_analyzer/issues_aggregation.rb
@@ -45,6 +45,43 @@ module Issues_Aggregation
 		return output
 	end
 
+	def self.get_issue_time(repo, issueNumber)
+		totalIssueSpentHoursBreakdown = Mongo_Connection.aggregate_test([
+			{"$project" => {type: 1, 
+							issue_number: 1, 
+							_id: 1, 
+							repo: 1,
+							milestone_number: 1, 
+							issue_state: 1, 
+							issue_title: 1, 
+							time_tracking_commits:{ duration: 1, 
+													type: 1, 
+													comment_id: 1 }}},
+			{ "$match" => { repo: repo }},			
+			{ "$match" => { type: "Issue" }},
+			{ "$match" => {issue_number: issueNumber}},
+			{ "$unwind" => "$time_tracking_commits" },
+			{ "$match" => { "time_tracking_commits.type" => { "$in" => ["Issue Time"] }}},
+			{ "$group" => { _id: {
+							repo_name: "$repo",
+							milestone_number: "$milestone_number",
+							issue_number: "$issue_number",
+							issue_title: "$issue_title",
+							issue_state: "$issue_state", },
+							time_duration_sum: { "$sum" => "$time_tracking_commits.duration" },
+							time_comment_count: { "$sum" => 1 }
+							}}
+							])
+		output = []
+		totalIssueSpentHoursBreakdown.each do |x|
+			x["_id"]["time_duration_sum"] = x["time_duration_sum"]
+			x["_id"]["time_comment_count"] = x["time_comment_count"]
+			output << x["_id"]
+		end
+		return output
+	end
+
+
 
 	def self.analyze_issue_spent_hours_per_user(repo, issueNumber)
 
