6c2aba7b763e412feac4bbc0eeadc3fb6cc157c7
Stephen Russett <stephenrussett@gmail.com>
2014-03-16 03:43:00 -0400

    added new method to get all issues in a specific milestone

--- time_analyzer/issues_processor.rb
+++ time_analyzer/issues_processor.rb
@@ -41,4 +41,26 @@ module Issues_Processor
     end
 
 
+
+    def self.get_issues_in_milestone(user, repo, milestoneNumber, githubAuthInfo)
+      userRepo = "#{user}/#{repo}"
+      Issues_Aggregation.controller
+      spentHours = Issues_Aggregation.get_all_issues_time_in_milestone(userRepo, milestoneNumber, githubAuthInfo)
+      budgetHours = Issues_Aggregation.get_all_issues_budget_in_milestone(userRepo, milestoneNumber, githubAuthInfo)
+      issues = Helpers.merge_issue_time_and_budget(spentHours, budgetHours)
+      issues.each do |x|
+        if x["time_duration_sum"] != nil
+          x["time_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["time_duration_sum"], "long")
+        end
+        if x["budget_duration_sum"] != nil
+          x["budget_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["budget_duration_sum"], "long")
+        end
+      end
+
+      issues = self.process_issues_for_budget_left(issues)
+      return issues
+
+    end
+
+
 end
