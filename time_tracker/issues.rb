58517b873a7385bcfde6dc08fce9633ae3d3e030
Stephen Russett <stephenrussett@gmail.com>
2015-11-20 23:45:42 -0500

    cleanup

--- time_tracker/issues.rb
+++ time_tracker/issues.rb
@@ -3,12 +3,10 @@ require_relative 'helpers'
 require_relative 'issue_budget'
 require_relative 'issue_time'
 require_relative '../gh_issue_task_aggregator'
-require 'pp'
 
 module Gh_Issue
 
 	def self.process_issue(repo, issueDetails, issueComments, githubAuthInfo)
-		
 		githubUserName = githubAuthInfo[:username]
 		githubUserID = githubAuthInfo[:userID]
 		issueState = issueDetails.attrs[:state]
@@ -18,14 +16,14 @@ module Gh_Issue
 		issueClosedAt = issueDetails.attrs[:closed_at]
 		issueLastUpdatedAt = issueDetails.attrs[:updated_at]
 		recordCreationDate = Time.now.utc
-		
+
 		# gets the milestone number assigned to a issue.  Is not milestone assigned then returns nil
 		milestoneNumber = Helpers.get_issue_milestone_number(issueDetails.attrs[:milestone])
-		
+
 		# gets labels data for issue and returns array of label strings
 		labelNames = Labels_Processor.get_label_names(issueDetails.attrs[:labels])
-		
-		# runs the label names through a parser to create Label categories.  
+
+		# runs the label names through a parser to create Label categories.
 		# used for advanced label grouping
 		labels = Labels_Processor.process_issue_labels(labelNames)
 
@@ -36,7 +34,7 @@ module Gh_Issue
 		commentsArray = []
 		# end of part of the task listing code
 
-		# cycles through each comment and returns time tracking 
+		# cycles through each comment and returns time tracking
 		issueComments.each do |x|
 			# checks to see if there is a time comment in the body field
 			isTimeComment = Helpers.time_comment?(x.attrs[:body])
@@ -45,7 +43,7 @@ module Gh_Issue
 				# if true, the body field is parsed for time comment details
 				parsedTime = Gh_Issue_Time.process_issue_comment_for_time(x)
 				if parsedTime != nil
-					# assuming results are returned from the parse (aka the parse was preceived 
+					# assuming results are returned from the parse (aka the parse was preceived
 					# by the code to be sucessful, the parsed time comment details array is put into
 					# the commentsTime array)
 					commentsTime << parsedTime
@@ -68,14 +66,14 @@ module Gh_Issue
 			if commentHasTasksTF == true
 				gotTasks = GH_Issue_Task_Aggregator.get_tasks_from_comment(commentBody1)
 				gotCommentDetails = GH_Issue_Task_Aggregator.get_comment_details(x)
-				
+
 				mergedDetails = GH_Issue_Task_Aggregator.merge_details_and_tasks(gotCommentDetails, gotTasks)
-				
+
 				commentsArray << mergedDetails
 			end
-				
+
 		end # do not delete this 'end'.  it is part of issueComments do block
-		
+
 		if commentsArray.empty? == false
 			gotIssueDetails = GH_Issue_Task_Aggregator.get_issue_details(repo, issueDetails)
 			gotIssueDetails["comments_with_tasks"] = commentsArray
